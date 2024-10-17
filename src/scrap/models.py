990e8ae0ae570b69364810f12d5aa60b0d769aec
Ilya <ilyapavlov200311@gmail.com>
2024-10-18 01:12:49 +0300

    Создал класс Lang для ЯП, а также сделал автоматическое формирование и сохранение slug

--- src/scrap/models.py
+++ src/scrap/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from pytils.translit import slugify
 
 
 class City(models.Model):
-    name = models.CharField(max_length=40, verbose_name="Название города")
-    slug = models.CharField(max_length=12, blank=True)
+    name = models.CharField(max_length=40, verbose_name="Название города", unique=True)
+    slug = models.CharField(max_length=12, blank=True, unique=True)
 
     def __str__(self):
         return self.name
@@ -11,3 +12,21 @@ class City(models.Model):
     class Meta:
         verbose_name_plural = "Города"
         verbose_name = "Город"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+
+        super().save(*args, **kwargs)
+
+
+class Lang(models.Model):
+    name = models.CharField(max_length=40, verbose_name="Язык программирования", unique=True)
+    slug = models.CharField(max_length=12, blank=True, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Языки программирования"
+        verbose_name = "Язык программирования"
