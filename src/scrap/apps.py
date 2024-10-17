8ab18254bcdaadcaf72a6b18ba0fe5a2aaa93c1e
Ilya <ilyapavlov200311@gmail.com>
2024-10-17 14:04:09 +0300

    Update visual of app "Scrap" in admin panel

--- src/scrap/apps.py
+++ src/scrap/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ScrapConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scrap'
+    verbose_name = 'Агрегатор вакансий'
