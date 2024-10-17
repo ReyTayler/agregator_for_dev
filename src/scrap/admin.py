f7754580bca7a5175513ca7ad583b2d003e80285
Ilya <ilyapavlov200311@gmail.com>
2024-10-18 01:11:31 +0300

    Добавил классы для удобного отображения полей в админке

--- src/scrap/admin.py
+++ src/scrap/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import City
+from .models import City, Lang
 
-admin.site.register(City)
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug")
+
+
+class LangAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug")
+
+
+admin.site.register(City, CityAdmin)
+admin.site.register(Lang, LangAdmin)
