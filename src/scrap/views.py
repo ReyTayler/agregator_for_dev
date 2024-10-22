c5ad5557dfb3993552552f62e6fa2b3af4589f7b
Ilya <ilyapavlov200311@gmail.com>
2024-10-23 02:22:38 +0300

    Добавил вьюху для url \home

--- src/scrap/views.py
+++ src/scrap/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Vacancy, City
 
-# Create your views here.
+
+def main_view(request):
+    vacancies = Vacancy.objects.all()
+
+    return render(request=request,
+                  template_name='scrap/home.html',
+                  context={"object_list": vacancies})
