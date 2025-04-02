21340f50b21cae69a23cbc28b9c17ec9a2e98739
Ilya <ilyapavlov200311@gmail.com>
2025-04-03 02:04:39 +0300

    Регистрация новых моделей в админке

--- src/scrap/admin.py
+++ src/scrap/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
-from .models import City, Lang, Vacancy
+from .models import City, Lang, Vacancy, Error, Url
 
 
 class CityAdmin(admin.ModelAdmin):
-    list_display = ("name", "slug")
+    list_display = ("id", "name", "slug")
 
 
 class LangAdmin(admin.ModelAdmin):
-    list_display = ("name", "slug")
+    list_display = ("id", "name", "slug")
 
 
 admin.site.register(City, CityAdmin)
 admin.site.register(Lang, LangAdmin)
 admin.site.register(Vacancy)
+admin.site.register(Error)
+admin.site.register(Url)
