012ba92a59c6baeb60cfbe4575736e7a095c5ce1
Ilya <ilyapavlov200311@gmail.com>
2025-04-08 01:40:04 +0300

    Удалил ненужное

--- src/scraping_service/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def home(request):
-    my_context = {'name': "Иван", "university": "КАИ"}
-
-    return render(request=request,
-                  template_name="Forms/index.html",
-                  context=my_context)
