f3421c4ca287074295017a25d5f5a7149d70ea5a
Ilya <ilyapavlov200311@gmail.com>
2025-04-08 01:40:26 +0300

    Добавление новых маршрутов

--- src/scraping_service/urls.py
+++ src/scraping_service/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from scrap.views import main_view
-
-from .views import home
+from scrap.views import main_view, vacancies_list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', main_view),
+    path('vacancies/', vacancies_list_view, name='vacancies'),
+    path('', main_view, name='main'),
 ]
