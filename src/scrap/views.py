0e53c28a2be4eb8f8393560612efb3a0c86aa877
Ilya <ilyapavlov200311@gmail.com>
2025-04-09 00:13:36 +0300

    Добавлена пагинация на сайт

--- src/scrap/views.py
+++ src/scrap/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.http import HttpRequest
 from .models import Vacancy
@@ -6,25 +7,35 @@ from .forms import FindVacancyForm
 
 def main_view(request: HttpRequest):
     form = FindVacancyForm()
-    return render(request=request, template_name='scrap/main.html', context={"form": form})
+    return render(
+        request=request, template_name="scrap/main.html", context={"form": form}
+    )
 
 
 def vacancies_list_view(request: HttpRequest):
-    city = request.GET.get('city')
-    lang = request.GET.get('lang')
+    city = request.GET.get("city")
+    lang = request.GET.get("lang")
 
     form = FindVacancyForm()
+
+    context = {"city": city, "lang": lang, "form": form}
 
     vacancies = []
     if city or lang:
         filter_for_vacancy = {}
         if city:
-            filter_for_vacancy['city__slug'] = city
+            filter_for_vacancy["city__slug"] = city
         if lang:
-            filter_for_vacancy['language__slug'] = lang
+            filter_for_vacancy["language__slug"] = lang
         vacancies = Vacancy.objects.filter(**filter_for_vacancy)
 
-    return render(request=request,
-                  template_name='scrap/vacancies.html',
-                  context={"object_list": vacancies,
-                           "form": form})
+    paginator = Paginator(vacancies, per_page=14)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context["object_list"] = page_obj
+    return render(
+        request=request,
+        template_name="scrap/vacancies.html",
+        context=context,
+    )
