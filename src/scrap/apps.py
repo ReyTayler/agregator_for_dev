d38c6e7cfe34777e8736152eae1ecb7f30910cac
Ilya <ilyapavlov200311@gmail.com>
2025-05-10 03:02:47 +0300

    Добавление запуска фоновых задач при готовности к работе приложения scrap

--- src/scrap/apps.py
+++ src/scrap/apps.py
@@ -1,3 +1,4 @@
+import os
 from django.apps import AppConfig
 
 
@@ -5,3 +6,10 @@ class ScrapConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scrap'
     verbose_name = 'Агрегатор вакансий'
+
+    def ready(self):
+        if os.environ.get('RUN_MAIN', None) != 'true':
+            return  # предотвращаем двойной запуск при autoreload
+
+        from .scheduler import start
+        start()
